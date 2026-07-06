5de1300286b15a471ead11aa5e85b2560b2ce55a
PavlovaElina <ellopavlova@gmail.com>
2026-07-06 15:35:55 +0300

    Initial Commit

---
+++ AuraDesk/AuraDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraDeskApp.swift
+//  AuraDesk
+//
+//  Created by Elina Pavlova on 06.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
