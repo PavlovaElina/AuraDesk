62a5aad620a8926486b70fff9b0f68570cfd7c34
PavlovaElina <ellopavlova@gmail.com>
2026-07-06 18:57:07 +0300

    feat: add draggable widgets

--- AuraDesk/ContentView.swift
+++ AuraDesk/ContentView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    let widgets: [WidgetItem] = [
+    @State private var widgets: [WidgetItem] = [
         WidgetItem(
             type: .clock,
-            x: 0,
-            y: 0,
+            x: 100,
+            y: 75,
             width: 300,
             height: 170
         )
@@ -13,14 +13,42 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            ForEach(widgets) { widget in
-                WidgetContainerView(widget: widget)
-                    .position(x: widget.x + widget.width / 2,
-                              y: widget.y + widget.height / 2)
+            ForEach($widgets) { $widget in
+                DraggableWidgetView(widget: $widget)
             }
         }
-        .frame(width: 500, height: 320)
+        .frame(width: 700, height: 450)
         .background(Color.clear)
+    }
+}
+
+struct DraggableWidgetView: View {
+    @Binding var widget: WidgetItem
+    @State private var dragStartX: Double?
+    @State private var dragStartY: Double?
+
+    var body: some View {
+        WidgetContainerView(widget: widget)
+            .position(
+                x: widget.x + widget.width / 2,
+                y: widget.y + widget.height / 2
+            )
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        if dragStartX == nil {
+                            dragStartX = widget.x
+                            dragStartY = widget.y
+                        }
+
+                        widget.x = (dragStartX ?? widget.x) + value.translation.width
+                        widget.y = (dragStartY ?? widget.y) + value.translation.height
+                    }
+                    .onEnded { _ in
+                        dragStartX = nil
+                        dragStartY = nil
+                    }
+            )
     }
 }
 
